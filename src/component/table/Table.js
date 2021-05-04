aa1b80a17095c5fb6277f24de315998436f835a6
VladimirAlexeev <alxeewladmr@gmail.com>
2021-05-04 20:07:03 +0300

    Events listners added in table js

--- src/component/table/Table.js
+++ src/component/table/Table.js
@@ -3,7 +3,32 @@ import { createTable } from './table.template';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
+
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+    })
+  }
+
+
   toHTML() {
     return createTable()
+  }
+
+  onClick() {
+    console.log('onClick')
+  }
+
+  onMousedown(event) {
+    console.log('onMousedown: ', event.target)
+  }
+
+  onMousemove() {
+    console.log('onMousemove')
+  }
+
+  onMouseup() {
+    console.log('onMouseup')
   }
 }
