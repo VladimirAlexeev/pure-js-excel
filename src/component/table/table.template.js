17519bfbfc63447e5e0902d0f19be5a476dd09d8
VladimirAlexeev <alxeewladmr@gmail.com>
2021-05-23 00:08:01 +0300

    store column resize width in local storage

--- src/component/table/table.template.js
+++ src/component/table/table.template.js
@@ -3,14 +3,21 @@ const CODES = {
   Z: 90
 }
 
+const DEFAULT_WIDTH = 120
+
+function getWidth(state, index) {
+  return (state[index] || DEFAULT_WIDTH) + 'px'
+}
+
 // function toCell(row, col) {
 //   return `
 //     <div class='cell' contenteditable data-col="${col}" data-row="${row}"></div>
 //   `
 // }
 
-function toCell(row) {
+function toCell(state, row) {
   return function(_, col) {
+    const width = getWidth(state.colState, col);
     return `
       <div
         class='cell'
@@ -18,14 +25,15 @@ function toCell(row) {
         data-col="${col}"
         data-type="cell"
         data-id="${row}:${col}"
+        style="width: ${width}"
       ></div>
     `
   }
 }
 
-function toColumn(col, index) {
+function toColumn({col, index, width}) {
   return `
-    <div class='column' data-type="resizable" data-col="${index}">
+    <div class='column' data-type="resizable" data-col="${index}" style="width: ${width}">
       ${col}
       <div class="col-resize" data-resize="col"></div>
     </div>
@@ -50,7 +58,15 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowsCount = 15) {
+function withWidthFrom(state) {
+  return function(col, index) {
+    return {
+      col, index, width: getWidth(state.colState, index)
+    }
+  }
+}
+
+export function createTable(rowsCount = 15, state = {}) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
 
@@ -60,7 +76,8 @@ export function createTable(rowsCount = 15) {
     //   return String.fromCharCode(CODES.A + index)
     // }) OR
     .map(toChar)
-    .map(toColumn) // index
+    .map(withWidthFrom(state))
+    .map(toColumn)
     .join('')
 
   rows.push(createRow(cols))
@@ -69,7 +86,7 @@ export function createTable(rowsCount = 15) {
     const cells = new Array(colsCount)
         .fill('')
         // .map((_, col) => toCell(row, col))
-        .map(toCell(row))
+        .map(toCell(state, row))
         .join('')
     rows.push(createRow(cells, row + 1))
   }
--- src/index.js
+++ src/index.js
@@ -6,8 +6,14 @@ import { Table } from '@/component/table/Table'
 import './scss/index.scss'
 import { createStore } from './core/createStore'
 import { rootReducer } from './redux/rootReducer'
+import { storage } from './core/utils'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, storage('excel-state'));
+
+store.subscribe(state => {
+  console.log('App State: ', state);
+  storage('excel-state', state);
+})
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
